c9e1f4f505b20d7b317e787a82369d835eb4d136
Luke Rogers <luke@dmptr.com>
2021-03-02 01:00:10 +1300

    cache images

--- web/client/service.js
+++ web/client/service.js
@@ -2,7 +2,6 @@ import {registerRoute} from 'workbox-routing';
 import {StaleWhileRevalidate} from 'workbox-strategies';
 
 registerRoute(
-  ({request}) => request.destination === 'script' ||
-                 request.destination === 'style',
+  ({request}) => request.destination === 'script' || request.destination === 'style' || request.destination === 'image',
   new StaleWhileRevalidate()
 );
